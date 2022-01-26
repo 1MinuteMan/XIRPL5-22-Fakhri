9dc28d4642bb96419f897d3db1dcdd8eb8d4ed4b
unknown <arihauzan347@gmail.com>
2022-01-26 08:31:02 +0700

    first commit

--- resources/routes/web.php
+++ resources/routes/web.php
@@ -12,31 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home', [
-    "title" => "Home"
-    ]);
+Route::get('/', function(){
+    return view('home');    
 });
 
-
-Route::get('/about', function () {
+Route::get('/about', function(){
     return view('about',[
-        "title" => "About",
-        "nama" => "Muhammad Fakhri Hauzan",
-        "email" => "arihauzan347@gmail.com",
+        "nama"=>"Muhammad Fakhri Hauzan",
+        "email"=>"arihauzan347@gmail.com",
         "gambar"=>"dodogijo.png"
     ]);
 });
 
 Route::get('/gallery', function(){
-    return view('gallery',[
-        "title" => "Gallery" 
-    ]);
-});
-
-Route::get('/contacts', function(){
-    return view('contacts',[
-        "title" => "Contacts" 
-    ]);
+    return view('gallery');
 }); 
+
+
+
+
